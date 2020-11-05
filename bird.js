f6b2731e32821faae6632a5197d3109e57a0025f
kd-potato <71477065+kd-potato@users.noreply.github.com>
2020-11-05 15:14:32 -0800

    Bird movements

--- bird.js
+++ bird.js
@@ -3,7 +3,7 @@ class Bird extends Sprite {
     super();
     this.img = loadImage("images/seagull.gif");
     this.x = width * 2.5;
-    this.y = random(100, 275);
+    this.y = random(200, 600);
     this.radius = 55;
     this.speed = 5;
 
@@ -13,10 +13,10 @@ class Bird extends Sprite {
     push();
     scale(0.5);
     imageMode(CENTER);
-    image(this.img, this.x, this.y);
+    image(this.img, this.x, this.y + (sin(frameCount/50) * 300));
     // noFill();
     // stroke('red')
     // circle(this.x, this.y, this.radius * 2) //hitbox
     pop();
   }
-}
+} 
