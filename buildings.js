32d4b778838ed16cf7cf1b985a568b5301ddfbe2
kd-potato <71477065+kd-potato@users.noreply.github.com>
2020-11-05 14:54:44 -0800

    fixed buildings.js

--- buildings.js
+++ buildings.js
@@ -6,6 +6,7 @@ class Building extends Sprite {
     this.y = windowHeight;
     this.yConstant = 0;
     this.building = random(buildings);
+    // this.building = buildings[1];
     this.speed = 2.5;
     this.radius = 200;
 
@@ -18,7 +19,10 @@ class Building extends Sprite {
       // imageMode(CENTER)
       // noFill();
       // circle((this.x * 0.9523), this.y - 230 , this.radius * 2);
-      image(buildings[0], this.x * 0.9523, this.y - 470);
+      
+      //image(buildings[0], this.x * 0.9523, this.y - 470);
+      image(buildings[0], this.x, this.y - buildings[0].height);
+
       // circle(this.x, this.y - 255 , this.radius * 2);
       // noFill();
       // strokeWeight(4);
@@ -29,31 +33,36 @@ class Building extends Sprite {
     if (this.building === buildings[1]) {
       push();
       scale(0.5);
-      image(buildings[1], this.x * 2, this.y - 250);
+      // image(buildings[1], this.x * 2, this.y - 250);
+      image(buildings[1], this.x * 2, this.y * 2 - buildings[1].height);
       pop();
     }
     if (this.building === buildings[2]) {
       push();
       scale(0.4);
-      image(buildings[2], this.x * 2.5, this.y + 320);
+      // image(buildings[2], this.x * 2.5, this.y + 320);
+      image(buildings[2], this.x * 2.5, this.y * 2.5 - buildings[2].height);
       pop();
     }
     if (this.building === buildings[3]) {
       push();
       scale(1.5);
-      image(buildings[3], this.x * 0.667, this.y - 500);
+      // image(buildings[3], this.x * 0.667, this.y - 500);
+      image(buildings[3], this.x * 0.667, this.y * 0.667 - buildings[3].height);
       pop();
     }
     if (this.building === buildings[4]) {
       push();
       scale(0.45);
-      image(buildings[4], this.x * 2.222, this.y - 15);
+      // image(buildings[4], this.x * 2.222, this.y - 15);
+      image(buildings[4], this.x * 2.222, this.y * 2.222 - buildings[4].height);
       pop();
     }
     if (this.building === buildings[5]) {
       push();
       scale(0.4);
-      image(buildings[5], this.x * 2.5, this.y + 500);
+      // image(buildings[5], this.x * 2.5, this.y + 500);
+      image(buildings[5], this.x * 2.5, this.y  * 2.5 - buildings[5].height);
       pop();
     }
   }
